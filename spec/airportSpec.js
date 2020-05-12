35d20f7d2e9dbd092cc5a3801e0ba920094aff29
mbrad26 <mbrad26@gmail.com>
2020-05-12 09:38:56 +0100

    implemented isStormy

---
+++ spec/airportSpec.js
@@ -0,0 +1,41 @@
+'use strict';
+
+describe('Airport', function () {
+  var airport;
+  var plane;
+
+  beforeEach(function () {
+    airport = new Airport();
+    plane = jasmine.createSpyObj('plane', ['land']);
+  });
+
+  it('has no planes by default', function () {
+    expect(airport.planes()).toEqual([]);
+  });
+
+  it('can clear planes for landing', function () {
+    airport.clearForLanding(plane);
+
+    expect(airport.planes()).toEqual([plane]);
+  });
+
+  it('can clear planes for takeoff', function () {
+    airport.clearForLanding(plane);
+    airport.clearForTakeOff(plane);
+
+    expect(airport.planes()).toEqual([]);
+  });
+
+  it('can check for stormy conditions', function () {
+    expect(airport.isStormy()).toBeFalsey;
+  });
+
+  describe('under stormy conditions', function () {
+    it('does not clear planes for takeoff', function () {
+      spyOn(airport, 'isStormy').and.returnValue(true);
+
+      expect(function () { airport.clearForTakeOff(plane); })
+      .toThrowError('cannot takeoff during storm');
+    });
+  });
+});
---
+++ src/plane.js
@@ -0,0 +1,16 @@
+'use strict';
+
+class Plane {
+  constructor() {
+    this._location;
+  }
+
+  land(airport) {
+    airport.clearForLanding(this);
+    this._location = airport;
+  }
+
+  takeoff() {
+    this._location.clearForTakeOff();
+  }
+}
